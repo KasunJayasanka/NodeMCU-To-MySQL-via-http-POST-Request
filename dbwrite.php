85eb410210be6da2ce2837f1cf4883b5d30e7956
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-06-22 23:13:57 +0530

    Update dbwrite.php

--- dbwrite.php
+++ dbwrite.php
@@ -1,56 +1,95 @@
 <?php
-session_start();
-?>
 
-<?php
-$api_key_value = "tPmAT5Ab3j7F9";
+// Change port accordingly
+$servername = "eu-cdbr-west-03.cleardb.net";
 
-$apiKey= $sensor = $location = $sensorValue = $value2 = $value3 = "";
+// REPLACE with your Database name
+$dbname = "heroku_cc37febd15469e6";
+// REPLACE with Database user
+$username = "bc198622e1365c";
+// REPLACE with Database user password
+$password = "f0e73a72";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $apiKey = test_input($_POST["apiKey"]);
+// Keep this API Key value to be compatible with the ESP32 code provided in the project page.
+// If you change this value, the ESP32 sketch needs to match
+$api_key_value = 'tPmAT5Ab3j7F9';
+
+// keep the respnse payload
+$response = ['success' => false, 'massage' => 'Internal server error' ];
+// set the http response code
+$responseCode = 500; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+    // get request header in order to get api key
+    $header = apache_request_headers();
+
+    // get api key
+    $apiKey = $header['X-API-KEY'] ?? null; // shorthand operator IF X-API-KEY exist in header assing X-API-KEY value ELSE assign null
+
+
+    if($apiKey == $api_key_value) {
         
-    if($apiKey==$api_key_value){
-        
-        $sensorValue = test_input($_POST["sensorValue"]);
-       
-        $_SESSION['data'] = $sensorValue;
-     
-        
+        // get input json payload
+        $json = file_get_contents('php://input');
+        $json = json_decode($json, true);
+
+        // var_dump($json); return;
+
         // Create connection
-        include 'inc/connection.php';
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
         // Check connection
         if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
-        $sql = "INSERT INTO nodemcu_table (sensorValue)
-        VALUES ('" . $sensorValue . "')";
-        
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-              
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            $response['massage'] = 'Connection failed'.$conn->connect_error;
+            $responseCode = 500; // internal server error
+        } else {
+            // prepare sql connection
+            // ref -> https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+            $stmt = $conn->prepare("INSERT INTO nodemcu_table (sensorValue1,sensorValue2,sensorValue3,sensorValue4) VALUES (?,?,?,?);");
+
+            // get values form pay load
+            $sensorValue1 = (float)($json['sensorValue1'] ?? 0); 
+            $sensorValue2 = (float)($json['sensorValue2'] ?? 0); 
+            $sensorValue3 = (float)($json['sensorValue3'] ?? 0); 
+            $sensorValue4 = (float)($json['sensorValue4'] ?? 0);
+
+            // bind paramiters to sql statement
+            $stmt->bind_param(
+                'dddd',
+                $sensorValue1, 
+                $sensorValue2, 
+                $sensorValue3, 
+                $sensorValue4,
+            );
+
+            if ($stmt->execute() === true) {
+                $response['success'] = true;
+                $response['massage'] = 'Data inserted successfully';
+                $responseCode = 200; // response success
+            } else {
+                $response['massage'] = 'Error: ' . $sql .' '. $conn->error;
+                $responseCode = 500; // internal server error
+            }
+
+            $stmt->close();
         }
-        
-        //------------------------------------------------------------------------------------------------------------------------------------
-     
+
+        // Close the connection
         $conn->close();
-    }
-    else {
-        echo $apiKey."Wrong API Key provided.";
-    }
 
-}
-else {
-    echo "No data posted with HTTP POST.";
+    } else {
+        $response['massage'] = 'Wrong API Key provided!';
+        $responseCode = 401; // unauthorized request
+    } 
+
+
+} else {
+    $response['massage'] = 'Method Not Allowed';
+    $responseCode = 405; // Method Not Allowed
 }
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+// send response to client
+http_response_code($responseCode);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response);
+?>
